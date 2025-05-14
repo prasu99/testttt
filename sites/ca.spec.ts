6d3b50baba423686431ee3a575c06d5edd7ec297
prasu99 <131260249+prasu99@users.noreply.github.com>
2025-05-14 11:26:22 +0530

    Update ca.spec.ts

--- sites/ca.spec.ts
+++ sites/ca.spec.ts
@@ -27,46 +27,6 @@ const pages = [
     url: 'https://www.forbes.com/advisor/ca/business/',
     h1: 'Transform Your Small Business'
   },
-  {
-    title: 'Cash Back Credit Cards',
-    url: 'https://www.forbes.com/advisor/ca/credit-cards/best/cash-back/',
-    h1: 'Best Cash Back Credit Cards In Canada For 2025'
-  },
-  {
-    title: 'Mortgage Lenders',
-    url: 'https://www.forbes.com/advisor/ca/mortgages/best-mortgage-lenders/',
-    h1: 'Best Mortgage Lenders In Canada For 2025'
-  },
-  {
-    title: 'Mortgage Rates',
-    url: 'https://www.forbes.com/advisor/ca/mortgages/best-mortgage-rates-in-canada/',
-    h1: 'Best Mortgage Rates In Canada For 2025'
-  },
-  {
-    title: 'Personal Loans',
-    url: 'https://www.forbes.com/advisor/ca/personal-loans/best-personal-loans/',
-    h1: 'Best Personal Loans In Canada For 2025'
-  },
-  {
-    title: 'GIC Rates',
-    url: 'https://www.forbes.com/advisor/ca/banking/gic/best-gic-rates/',
-    h1: 'Best GIC Rates In Canada For 2025'
-  },
-  {
-    title: 'Savings Accounts',
-    url: 'https://www.forbes.com/advisor/ca/banking/savings/best-savings-accounts/',
-    h1: 'Best Savings Accounts In Canada For 2025'
-  },
-  {
-    title: 'Chequing Accounts',
-    url: 'https://www.forbes.com/advisor/ca/banking/chequing/best-chequing-accounts/',
-    h1: 'Best Chequing Accounts In Canada For 2025'
-  },
-  {
-    title: 'Travel Credit Cards',
-    url: 'https://www.forbes.com/advisor/ca/credit-cards/best/travel/',
-    h1: 'Best Travel Credit Cards In Canada For 2025'
-  }
 ];
 
 async function delay(ms: number) {
